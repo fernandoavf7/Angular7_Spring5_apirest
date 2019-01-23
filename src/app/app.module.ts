f489aa8ff5360c78ae7e8a1afbfe6ee7f4eb3f85
Fernando <fernando.vera@adevcom.cl>
2019-01-23 11:45:35 -0300

    avance union con spring 5 recibiendo json y agregando librerias necesarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ClientesComponent } from './clientes/clientes.component';
 import {ClienteService} from './clientes/cliente.service';
 //se importan RouterModule y routes para hacer el redireccionamiento
 import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 //se crea un array de rutas para redireccionar las urls
 //por defecto o home redirije a clientes
@@ -30,6 +31,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -5,17 +5,30 @@ import {CLIENTES} from './clientes.json';
 import {Cliente} from './cliente';
 //importa reactive en angular 5 o inferior usar  from 'rxjs/Observable'
 import{of, Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 
 //injectable representa logica de negocios, se puede injectar en otros archivos
 @Injectable({providedIn: 'root'})
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+
+  //se inyecta la dependencia httpclient en el constructor
+  constructor(private http: HttpClient) { }
 
   //debe retornar un Stream por lo tanto un Observable que contendra un listado de clientes
   //observables se suscriben al objeto y si existre un cambio se gatilla un proceso o tarea
   getclientes(): Observable<Cliente[]>{
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    
+    //castea el tipo "any" de angular a un array del objeto Cliente
+    //return this.http.get<Cliente[]>(this.urlEndPoint);
+
+    //otra forma de cast, mapeando el response al array de objeto Cliente
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
   }
 }
